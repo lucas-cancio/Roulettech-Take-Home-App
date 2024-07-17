e5eb74fa5b0bbf5d562af01cfe83935ad2a7c863
EC2 Default User <ec2-user@ip-172-31-92-200.ec2.internal>
2024-07-17 19:46:41 +0000

    Made backend-redeployment script migrate DB changes, collect static files, and restart gunicorn service

--- redeploy-backend.sh
+++ redeploy-backend.sh
@@ -4,16 +4,14 @@ git reset origin/main --hard
 source venv/bin/activate
 python3.12 -m pip install -r requirements.txt
 
-#cd frontend
-#npm run build
-
-#cd /home/ec2-user/Roulettech-Take-Home-App
+cd backend/
+python manage.py migrate
+python manage.py collectstatic
+cd /home/ec2-user/Roulettech-Take-Home-App
 
 # Set appropriate permissions for repo
 sudo chown -R ec2-user:nginx /home/ec2-user/Roulettech-Take-Home-App
 chmod -R 770 /home/ec2-user/Roulettech-Take-Home-App
 
-# Set appropriate permissions for React build files
-#chmod -R 750 /home/React-DRF-Task-Manager-App/frontend/build
-
-#sudo systemctl restart gunicorn
+# Restart gunicorn daemon
+sudo systemctl restart gunicorn
