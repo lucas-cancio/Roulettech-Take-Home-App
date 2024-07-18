3b3acd39d8029a098b2fce1a1e75ae934f93bf11
Lucas Cancio <lucas.canciox1@gmail.com>
2024-07-18 02:46:08 -0400

    RA-13 Created recipe model and endpoints
    -Created recipe model and serializer with attributes like name, cuisine, ingredients, instructions, rating, summary, and cook time.
    -Created to end points: one for accessing recipes as a list, and one for accessing specific recipes
    -React app retrieves and displays all recipes at the moment

---
+++ backend/recipes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    path('recipes/', views.RecipeListAPIView.as_view()),
+    path('recipes/<int:pk>', views.RecipeDetailAPIView.as_view()),
+]
