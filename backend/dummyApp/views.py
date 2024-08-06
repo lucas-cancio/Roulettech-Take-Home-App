6dc82f49d40ba9cc5f800af8b2388ad37cf776c2
Lucas Cancio <lucas.canciox1@gmail.com>
2024-08-06 18:03:02 -0400

    Made a change to the dummyApp API endpoint to test backend deployment automation

--- backend/dummyApp/views.py
+++ backend/dummyApp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hi there. Here is some dummy text from the dummyApp on the Django server")
+    return HttpResponse("Hi there. Here is some dummy text from the dummyApp on the Django server. I got automation working!")
