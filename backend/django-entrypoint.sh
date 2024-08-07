93d0451eb1b5871da781cd496b065660ed8238c5
Lucas Cancio <lucas.canciox1@gmail.com>
2024-08-07 16:41:26 -0400

    Testing whether web container name is backend_web_1 within container network

--- backend/django-entrypoint.sh
+++ backend/django-entrypoint.sh
@@ -11,7 +11,7 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
+#python manage.py flush --no-input
 python manage.py migrate
 python manage.py collectstatic --no-input
 #sudo chown -R ec2-user:nginx ./ && chmod -R 770 ./
